1fe7d894cb51a64dc49310d9c0ee2c48b352c8f4
vkit <rao.raghvendra41@gmail.com>
2018-01-27 20:31:30 +0530

    removed admin user page

--- street/management/commands/createsu.py
+++ street/management/commands/createsu.py
@@ -1,9 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
+# from django.core.management.base import BaseCommand, CommandError
+# from django.contrib.auth.models import User
 
-class Command(BaseCommand):
+# class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("raghu", "admin@admin.com", "raghu1234")
-            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
+#     def handle(self, *args, **options):
+#         if not User.objects.filter(username="admin").exists():
+#             User.objects.create_superuser("raghu", "admin@admin.com", "raghu1234")
+#             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
