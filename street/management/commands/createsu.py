50c7d41c8b22b98f3498fb4240b047e460644cde
vkit <rao.raghvendra41@gmail.com>
2018-01-27 17:55:30 +0530

    changed username is create.su

--- street/management/commands/createsu.py
+++ street/management/commands/createsu.py
@@ -5,5 +5,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("vimal", "admin@admin.com", "vimal1234")
+            User.objects.create_superuser("raghu", "admin@admin.com", "raghu1234")
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
